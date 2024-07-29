c95aa2573e876110217740a38817a1e97d43fab8
Zack Murry <zackk.murryy@gmail.com>
2024-07-28 20:29:08 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -31,6 +31,43 @@ if [ ! -f /opt/hadoop/initialized ] ; then
   mv /usr/src/app/yarn-site.xml .
   mv /usr/src/app/mapred-site.xml .
 
+  jav_test=$(readlink -f /usr/bin/java | sed "s:bin/java::")
+  echo $jav_test
+
+  sed -i -e "s:# export JAVA_HOME=:export JAVA_HOME=$jav_test:g" /opt/hadoop/etc/hadoop/hadoop-env.sh
+  echo "export HADOOP_SSH_OPTS=\"-p 30022 -o StrictHostKeyChecking=accept-new\"" >> /opt/hadoop/etc/hadoop/hadoop-env.sh
+
+  mkdir -p /home/hduser/.ssh
+  chown hduser:hadoop /home/hduser/.ssh
+  runuser -u hduser -- ssh-keygen -t rsa -b 4096 -f /home/hduser/.ssh/id_rsa -P ""
+  runuser -u hduser -- touch /home/hduser/.ssh/authorized_keys
+  echo "ls -la /home/hduser/.ssh"
+  runuser -u hduser -- ls -la /home/hduser/.ssh
+  runuser -u hduser -- chmod 700 /home/hduser/.ssh
+  runuser -u hduser -- chmod 644 /home/hduser/.ssh/id_rsa.pub
+  runuser -u hduser -- chmod 644 /home/hduser/.ssh/authorized_keys
+  runuser -u hduser -- chmod 600 /home/hduser/.ssh/id_rsa
+  runuser -u hduser -- chmod 600 /home/hduser/.ssh/id_rsa
+  runuser -u hduser -- chmod 755 /home/hduser
+  echo "ls -la /home/hduser/.ssh"
+  runuser -u hduser -- ls -la /home/hduser/.ssh
+
+
+  #rc-update add sshd
+  #rc-status
+  #rc-service sshd start
+
+  sed -i -e "s/#Port 22/Port 30022/g" /etc/ssh/sshd_config
+  sed -i -e "s/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/g" /etc/ssh/sshd_config
+  sed -i -e "s/#PasswordAuthentication yes/PasswordAuthentication yes/g" /etc/ssh/sshd_config
+  sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
+  #cat /etc/ssh/sshd_config
+  #runuser -u hduser -- ssh-keygen -A
+  #echo "ls /etc/ssh"
+  #ls /etc/ssh
+  #echo "Creating ssh hostkey"
+  ssh-keygen -A
+
 fi
 
 cd /opt/hadoop
